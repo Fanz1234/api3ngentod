6e7741b38a9f1ddd4554d6401b2f05e87f5aaa99
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2023-01-22 00:47:54 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api.fanz.me';
+var url = 'https://fanz01.ezyro.com';
 try {
-if(url == 'https://api.fanz.me') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://fanz01.ezyro.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
